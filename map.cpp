f2275ce4c216540873999163729fb6e3a9ff8643
Md. Ariful Islam <49308565+ashik255@users.noreply.github.com>
2023-03-12 22:44:53 +0600

    Add files via upload
     maps store the key values in ascending order by default.

---
+++ map.cpp
@@ -0,0 +1,40 @@
+/*
+Maps are associative containers that store elements
+formed by a combination of a key value and a mapped value,
+following a specific order.
+Each element associates a key to a mapped value*/ 
+#include <iostream>
+#include <map>
+
+using namespace std;
+int main()
+{ 
+// typedef pair<const Key, T> value_type;
+
+    map<int, string> m;
+
+    m[1] = "MD";
+    m[11] = "Ariful";
+    m[13] = "Islam";
+    m[10] = "Ashik";
+
+    m.insert({5, "khan"});
+
+    cout << "before Erase " << endl;
+    for (auto i : m)
+    {
+        cout << i.first << " " << i.second << endl;
+    }
+    cout << "finding -13 " << m.count(-13) << endl;
+    cout << "finding 11 " << m.count(11) << endl;
+    // cout<<"finding 13 "<<m.count(13)<<endl;
+
+    m.erase(13);
+    cout << "after Erase " << endl;
+    for (auto i : m)
+    {
+        cout << i.first << " " << i.second << endl;
+    }
+    cout << endl
+         << endl;
+}
